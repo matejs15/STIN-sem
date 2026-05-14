f19446d080448da0c03f50b45bf165ad83192d86
Matěj Svoboda <matej.svoboda2@tul.cz>
2026-05-14 05:25:56 +0200

    Initial commit

---
+++ src/main/java/com/example/stinsem/StinSemApplication.java
@@ -0,0 +1,13 @@
+package com.example.stinsem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StinSemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StinSemApplication.class, args);
+    }
+
+}
